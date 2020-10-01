dc7716a4f2d6b7704a28c5178a7c99ce990e58c7
Vito Breda <vito@ecocentauro.com.br>
2020-10-01 16:40:12 -0300

    teste flatlist

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Linking, ScrollView, TextInput} from 'react-native';
+import {Text, Linking, FlatList, TextInput} from 'react-native';
 import Clipboard from '@react-native-community/clipboard';
-import {Svg, Circle} from 'react-native-svg';
+import {Svg, Circle, Path} from 'react-native-svg';
 import {gyms} from './gyms';
 import * as S from './styles';
 
 export default function App() {
   const [filter, setFilter] = useState();
+  const [dados, setDados] = useState(gyms);
+
+  useEffect(() => {
+    filterItem();
+  }, [filter]);
 
   function openGps(coordinates) {
     var url = 'http://www.google.com/maps/place/' + coordinates;
@@ -31,31 +36,18 @@ export default function App() {
     }
   }
 
+  function filterItem() {
+    setDados(gyms.filter(handleFilter));
+  }
+
   function DisplayInfo({caption, value}) {
     return (
       <S.Wrapper>
         <S.Title>{caption}</S.Title>
-        <S.Info
-          style={{color: parseInt(value) < 0 ? '#E65D4E' : '#4B6E8B'}}
-          numberOfLines={1}>
-          {value}
-        </S.Info>
+        <S.Info numberOfLines={1}>{value}</S.Info>
       </S.Wrapper>
     );
   }
-
-  /*<S.CircleContainer>
-    <Svg
-      style={{
-        width: 50,
-        height: 50,
-      }}>
-      <S.CircleValueWrapper>
-        <S.TextIdClient>{item.codigo}</S.TextIdClient>
-      </S.CircleValueWrapper>
-      <Circle cx="25" cy="25" r="25" fill="#4B6E8B" />
-    </Svg>
-  </S.CircleContainer>;*/
 
   function CustomButtom({onPress, title}) {
     return (
@@ -65,46 +57,67 @@ export default function App() {
     );
   }
 
-  function renderCard() {
-    return gyms.filter(handleFilter).map((item, index) => {
-      return (
-        <S.CardComponent key={index}>
-          <S.RowComponent>
-            <S.CircleContainer>
-              <Svg
+  const renderItem = ({item}) => (
+    <S.CardComponent>
+      <S.RowComponent>
+        <S.CircleContainer>
+          <Svg
+            style={{
+              width: 50,
+              height: 50,
+            }}>
+            <S.CircleValueWrapper>
+              <Text
                 style={{
-                  width: 50,
-                  height: 50,
+                  color: '#F0F0F0',
+                  fontFamily: 'Roboto-Regular',
+                  fontSize: 18,
                 }}>
-                <S.CircleValueWrapper>
-                  <Text>{item.id}</Text>
-                </S.CircleValueWrapper>
-                <Circle cx="25" cy="25" r="25" fill="#4B6E8B" />
-              </Svg>
-            </S.CircleContainer>
-          </S.RowComponent>
-          <S.RowComponent>
-            <DisplayInfo
-              caption="Cordenadas"
-              value={item.coordinates}></DisplayInfo>
-          </S.RowComponent>
-          <CustomButtom
-            title={'Como chegar?'}
-            onPress={() => openGps(item.coordinates)}></CustomButtom>
-        </S.CardComponent>
-      );
-    });
-  }
+                {item.id}
+              </Text>
+            </S.CircleValueWrapper>
+            <Circle cx="25" cy="25" r="25" fill="#303030" />
+          </Svg>
+        </S.CircleContainer>
+
+        <S.WrapperTitle>
+          <DisplayInfo caption="Nome" value={item.name} />
+        </S.WrapperTitle>
+      </S.RowComponent>
+      <S.RowComponent>
+        <S.Wrapper>
+          <DisplayInfo caption="Cordenadas" value={item.coordinates} />
+        </S.Wrapper>
+        <S.BtnCopy onPress={() => Clipboard.setString(item.coordinates)}>
+          <Svg width="20" height="20" x="0" y="0" viewBox="0 0 512 512">
+            <Path
+              fill="#f0f0f0"
+              d="m271 512h-191c-44.113281 0-80-35.886719-80-80v-271c0-44.113281 35.886719-80 80-80h191c44.113281 0 80 35.886719 80 80v271c0 44.113281-35.886719 80-80 80zm-191-391c-22.054688 0-40 17.945312-40 40v271c0 22.054688 17.945312 40 40 40h191c22.054688 0 40-17.945312 40-40v-271c0-22.054688-17.945312-40-40-40zm351 261v-302c0-44.113281-35.886719-80-80-80h-222c-11.046875 0-20 8.953125-20 20s8.953125 20 20 20h222c22.054688 0 40 17.945312 40 40v302c0 11.046875 8.953125 20 20 20s20-8.953125 20-20zm0 0"
+            />
+          </Svg>
+        </S.BtnCopy>
+      </S.RowComponent>
+      <CustomButtom
+        title={'Como chegar?'}
+        onPress={() => openGps(item.coordinates)}></CustomButtom>
+    </S.CardComponent>
+  );
 
   return (
-    <ScrollView>
+    <S.PageDefault>
       <S.FieldContainer>
         <TextInput
+          style={{color: '#C0C0C0'}}
           placeholder={'Pesquisar ...'}
+          placeholderTextColor="#C0C0C0"
           onChangeText={(text) => setFilter(text)}
           value={filter}></TextInput>
       </S.FieldContainer>
-      {renderCard()}
-    </ScrollView>
+      <FlatList
+        data={dados}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString() + item.coordinates}
+      />
+    </S.PageDefault>
   );
 }
--- styles.js
+++ styles.js
@@ -5,6 +5,12 @@ const {width, height} = Dimensions.get('screen');
 
 const space = width < 430 ? 5 : 8;
 
+export const PageDefault = styled.View`
+  flex: 1;
+  padding: 10px;
+  background-color: #101010;
+`;
+
 export const metrics = {
   cardPadding: 10,
   cardBorderRadius: space,
@@ -14,12 +20,11 @@ export const metrics = {
 };
 
 export const CardComponent = styled.View`
-  flex: 1;
   flex-direction: column;
   padding: ${metrics.cardPadding}px;
   margin: ${metrics.cardMargin}px;
   border-radius: ${metrics.cardBorderRadius}px;
-  background: #ffffff 0% 0% no-repeat padding-box;
+  background: #242424 0% 0% no-repeat padding-box;
   box-shadow: 3px 3px 6px #00000029;
   elevation: 3;
 `;
@@ -29,10 +34,15 @@ export const Wrapper = styled.View`
   margin: 5px 0 5px 0;
 `;
 
+export const WrapperTitle = styled.View`
+  flex: 1;
+  margin: 5px 10px 5px 10px;
+`;
+
 export const Title = styled.Text`
   text-align: left;
   font-size: 14px;
-  color: #4b6e8b;
+  color: #c0c0c0;
   margin-bottom: 5px;
   font-family: Roboto-Regular;
 `;
@@ -41,7 +51,7 @@ export const Info = styled.Text`
   text-align: left;
   font-size: 16px;
   font-weight: 600;
-  color: #042043;
+  color: #f0f0f0;
   text-transform: capitalize;
   font-family: Roboto-Bold;
 `;
@@ -52,15 +62,16 @@ export const RowComponent = styled.View`
 `;
 
 export const FieldContainer = styled.View`
-  flex: 1;
+  height: 40px;
   border: 0.2px;
-  padding: 0;
+  padding: 0 10px 0 10px;
+  border-color: #34725b;
   margin: 15px 10px 5px 10px;
 `;
 
 export const ContainerButtom = styled.TouchableOpacity`
   height: 48px;
-  background-color: #4b6e8b;
+  background-color: #34725b;
   border-radius: 5px;
   align-items: center;
   justify-content: center;
@@ -75,8 +86,7 @@ export const TextButtom = styled.Text`
 
 export const CircleContainer = styled.View`
   height: 100%;
-  margin: 10px;
-  margin: 0;
+  margin-top: 10px;
   padding: 0;
 `;
 
@@ -87,4 +97,8 @@ export const CircleValueWrapper = styled.View`
   align-items: center;
   margin: 0;
   padding: 0;
+`;
+
+export const BtnCopy = styled.TouchableOpacity`
+  margin: 20px 30% 10px 10px;
 `;
